a6b2f4c82c7148bc11468876c44672422cacd7b5
bmwasaru <bmwasaru@gmail.com>
2014-12-11 19:39:41 +0300

    configure mongo db

--- events/__init__.py
+++ events/__init__.py
@@ -1,6 +1,11 @@
 from flask import Flask
+from flask.ext.mongoengine import MongoEngine
 
 app = Flask(__name__)
+app.config["MONGODB_SETTINGS"] = {'DB': 'events'}
+app.config["SECRET_KEY"] = '645c3ae11703#571cda41*10985861be*7c133f03'
+
+db = MongoEngine(app)
 
 if __name__ == '__main__':
     app.run()
